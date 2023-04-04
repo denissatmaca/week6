4785deb6ec9cd42689019cada2c19b56e73f4035
deniz atmaca <deniz.atmaca@outlook.com>
2023-04-04 04:51:21 +0300

    week 6

---
+++ arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket.");
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+
+
+fruitBasket[0] = "pine-apple";
+fruitBasket[1] = "peaches";
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}    
+
---
+++ week6stringsandarrays/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Diagnostics.Metrics;
+
+string userName = "Julia";
+
+
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The first letter in your name is {userName[userName.Length - 1]}.");
+
+////a-counter
+
+//int counter = 0;    
+
+//for(int i = 0; i < userName.Length; i++)
+//{
+//    if (userName[i] == 'a')
+//    {
+//        counter++; //counter = counter + 1
+//    }
+//}
+//Console.WriteLine($"Your first name has {counter} a-letters");
+
+
+//replace
+//a - 4 
+
+string updatedUserName = userName.Replace('a', '4').Replace('e', '3');
+
+Console.WriteLine(updatedUserName);
